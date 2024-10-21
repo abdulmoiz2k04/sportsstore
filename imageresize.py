980d202d17a50b9f56503816724beb3a0d3de9e7
Abdul Moiz <124672178+abdulmoiz2k04@users.noreply.github.com>
2024-10-21 19:57:58 +0500

    Delete imageresize.py

--- imageresize.py
+++
@@ -1,6 +0,0 @@
-import PIL
-from PIL import Image
-
-img = Image.open('D:\Abdul Moiz\scholarship app\Affidavit2.jpeg')
-img = img.resize((700, 1000), PIL.Image.Resampling.LANCZOS)
-img.save('Compressed1.jpg')
